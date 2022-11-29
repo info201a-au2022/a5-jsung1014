5ac8a79039e65eb7021acc090f2ad507f7f40a03
Ji Min Sung <jsung@Jis-MacBook-Pro-2.local>
2022-11-29 10:40:22 -0800

    updated

---
+++ app_server.R
@@ -0,0 +1,55 @@
+library("shiny")
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+library("plotly")
+library("rsconnect")
+
+# loading dataset
+df<- read.csv("./data/owid-co2-data.csv")
+
+us_co2 <- df %>% 
+  filter(country == "United States") %>%
+  mutate(total_co2 = sum(co2, na.rm = TRUE))%>%
+  summarize(average_co2 = mean(total_co2))%>%
+  pull(average_co2)
+
+us_co2_2020 <- df %>% 
+  filter(year == 2020) %>% 
+  filter(country == "United States") %>% 
+  pull(co2)
+
+world_avg_co2_before_2000<- df %>%
+  filter(year > 1980, year < 2000) %>%
+  filter(country == "World") %>%
+  mutate(total_co2 = sum(co2, na.rm = TRUE))%>%
+  summarize(average_co2 = mean(total_co2))%>%
+  pull(average_co2)
+
+world_avg_co2_after_2000 <- df %>%
+  filter(year > 2000) %>%
+  filter(country == "World") %>%
+  mutate(total_co2 = sum(co2, na.rm = TRUE))%>%
+  summarize(average_co2 = mean(total_co2))%>%
+  pull(average_co2)
+
+
+# Define server logic
+server <- function(input, output){
+  
+  plot <- function(country,year){
+    data_for_plot <- df %>%
+      filter(country %in% input$country) %>% 
+      filter(`year` >= input$year[1] & `year` <= input$year[2])
+    return(data_for_plot)
+  }
+  output$chart <- renderPlotly({
+    ggplotly(ggplot(plot(input$country & input$year),aes(x= `year`, y= `co2`)) +
+               geom_line(color = "red") +
+               labs(title = input$country, x = "year", y = "co2"))
+  })
+}
+
+
+
+
---
+++ app_ui.R
@@ -0,0 +1,55 @@
+library("shiny")
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+library("plotly")
+library("rsconnect")
+
+# loading dataset
+df<- read.csv("./data/owid-co2-data.csv")
+
+intro_view <- tabPanel(
+  "introduction",
+  h1(strong("Global Warming Cause")),
+  img("", src = "https://cdn.vox-cdn.com/thumbor/8etKO18iyGNYMjYcaOQXZbC5jv0=/3000x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/13439425/shutterstock_285980522.jpg",
+      width = "600", height = "300", align = "center"),
+  p("In the post-industrial era (1850-1900), humans started burning fossil fuels, which increased levels of heat-trapping greenhouse gases in Earth's atmosphere, resulting in long-term warming of Earth's surface.
+    The one of the main causes is releasing too much carbon dioxide. Because carbon dioxide in the atmosphere warms the planet, causing climate change. Nowdays, the majority of countries in the world keep releasing 
+    untold amount of carbon dioxide. Getting know what the cause of global warming is really important task to figure out the solution"),
+  h2(strong("Data Anaysis")),
+  p("The United States co2 in average: ", us_co2),
+  p("The United States co2 in 2020: ", us_co2_2020),
+  p("The World average c02 before 2000: ", world_avg_co2_before_2000),
+  p("The World average c02 after 2000: ", world_avg_co2_after_2000)
+)
+
+chart_view <- tabPanel(
+  "Carbon Dioxide",
+  h1(strong("CO2 per country")),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "country",label = "Select Country:",
+        choices = unique(df$country), selected = NULL
+      ),
+      sliderInput(
+        inputId = "year", label = "Select year:",
+        min = 1920, max = 2020, value = c(1920, 2020)
+      ),
+      h3(strong("Common Result")),
+      p("The majority of countries in the dataset are increasing the total production-based emissions of carbon dioxide (CO₂)"),
+    ),
+    mainPanel(
+      plotlyOutput("chart")
+    )
+  )
+)
+
+# Define UI
+ui <- navbarPage(
+  "Global Warming Cause",
+  theme = shinythemes::shinytheme("cosmo"),
+  intro_view,
+  chart_view
+)
+
